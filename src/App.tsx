dd663501519c02a29dfa79aef1d694c4c0c96114
Vertori <115083330+Vertori@users.noreply.github.com>
2023-07-03 11:27:58 +0200

    website scheme /w routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,33 @@
-import './App.css'
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// pages
+import Home from "./pages/Home";
+import ProductDetails from "./pages/ProductDetails";
+import WomensClothing from "./pages/WomensClothing";
+import MensClothing from "./pages/MensClothing";
+import Jewelery from "./pages/Jewelery";
+// components
+import Sidebar from "./components/Sidebar";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
-
-
   return (
-    <>
-
-    </>
-  )
+    <div className="overflow-hidden">
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/product/:id" element={<ProductDetails />} />
+          <Route path="/women" element={<WomensClothing />} />
+          <Route path="/men" element={<MensClothing />} />
+          <Route path="/jewelery" element={<Jewelery />} />
+        </Routes>
+        <Sidebar />
+        <Footer />
+      </Router>
+    </div>
+  );
 }
 
-export default App
+export default App;
