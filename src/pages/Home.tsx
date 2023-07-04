c3bec20558c8045c6736eb75af7b5ed36044b62a
Vertori <115083330+Vertori@users.noreply.github.com>
2023-07-04 11:42:26 +0200

    home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,34 @@
-import { useContext } from 'react'
-import { ProductContext } from '../contexts/ProductContext'
+import { useContext } from "react";
+import { ProductContext } from "../contexts/ProductContext";
+import Hero from "../components/Hero";
+import Product from "../components/Product";
 
 const Home = () => {
-  const products = useContext(ProductContext)
-  console.log(products)
+  const products = useContext(ProductContext);
+
+  // getting only men's and women's clothing
+  const filteredProducts = products?.filter((item) => {
+    return (
+      item.category === "men's clothing" ||
+      item.category === "women's clothing" ||
+      item.category === "jewelery"
+    );
+  });
 
   return (
-    <div></div>
-  )
-}
+    <div>
+      <Hero />
+      <section className="py-16 border-b">
+        <div className="container mx-auto">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-[30px] max-w-sm md:max-w-none mx-auto md:mx-0">
+            {filteredProducts?.map((product) => {
+              return <Product product={product} key={product.id} />;
+            })}
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
