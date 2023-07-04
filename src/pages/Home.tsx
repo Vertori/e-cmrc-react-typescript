05eacc6f904caeaaee42c086c7f0a6d2986e7684
Vertori <115083330+Vertori@users.noreply.github.com>
2023-07-04 13:00:34 +0200

    product component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,7 @@ import { ProductContext } from "../contexts/ProductContext";
 import Hero from "../components/Hero";
 import Product from "../components/Product";
 
-const Home = () => {
+const Home = (): JSX.Element => {
   const products = useContext(ProductContext);
 
   // getting only men's and women's clothing
@@ -21,8 +21,8 @@ const Home = () => {
       <section className="py-16 border-b">
         <div className="container mx-auto">
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-[30px] max-w-sm md:max-w-none mx-auto md:mx-0">
-            {filteredProducts?.map((product) => {
-              return <Product product={product} key={product.id} />;
+            {filteredProducts?.map((item) => {
+              return <Product product={item} key={item.id} />;
             })}
           </div>
         </div>
