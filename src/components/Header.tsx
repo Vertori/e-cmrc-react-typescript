566dc5e74b0d039a747c581d3dbfe5685bd9fb27
Vertori <115083330+Vertori@users.noreply.github.com>
2023-07-05 10:35:05 +0200

    open/close sidebar functionality

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { IoMdArrowForward } from "react-icons/io";
+import { FiTrash2 } from "react-icons/fi";
+import CartItem from "../components/CartItem";
+import { SidebarContext } from "../contexts/SidebarContext";
+// import { CartContext } from "../contexts/CartContext";
 
-const Header = () => {
+const Header = (): JSX.Element => {
+  const { isOpen, setIsOpen } = useContext(SidebarContext);
+
   return (
-    <div>Header</div>
-  )
-}
+    <header>
+      <div>Header</div>
+      <div onClick={() => setIsOpen(!isOpen)}>open/close sidebar</div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import ProductProvider from './contexts/ProductContext.tsx'
+import SidebarProvider from './contexts/SidebarContext.tsx'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <SidebarProvider>
 <ProductProvider>
     <React.StrictMode>
     <App />
   </React.StrictMode>
 </ProductProvider>
-
+</SidebarProvider>
 )
