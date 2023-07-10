d5decb4bb0b503e60ed9e87678cd59d58275ce1d
Vertori <115083330+Vertori@users.noreply.github.com>
2023-07-10 11:34:41 +0200

    men's clothing page

--- src/pages/MensClothing.tsx
+++ src/pages/MensClothing.tsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import { useContext } from "react";
+import { ProductContext } from "../contexts/ProductContext";
+import Product from "../components/Product";
+import CategorizedHero from "../components/CategorizedHero";
+import { BiChevronRight } from "react-icons/bi";
+import { AiOutlineHome } from "react-icons/ai";
+import { Link } from "react-router-dom";
 
 const MensClothing = () => {
-  return (
-    <div>MensClothing</div>
-  )
-}
+  const products = useContext(ProductContext);
 
-export default MensClothing
+  const filteredMensProducts = products?.filter((item) => {
+    return item.category === "men's clothing";
+  });
+
+  return (
+    <div>
+      <CategorizedHero category="Men's clothing" />
+      <section className="py-16 border-b ">
+        <div className="container mx-auto">
+          {/* location nav */}
+          <div className="pb-8 flex md:block">
+            <nav className="mx-auto px-4">
+              <ol className="flex items-center gap-1 text-sm text-gray-600">
+                <li>
+                  <Link to="/">
+                    <AiOutlineHome />
+                  </Link>
+                </li>
+                <li>
+                  <BiChevronRight />
+                </li>
+                <li>Men's clothing</li>
+              </ol>
+            </nav>
+          </div>
+          {/* displayed products */}
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-[30px] max-w-sm md:max-w-none mx-auto md:mx-0">
+            {filteredMensProducts?.map((product) => {
+              return <Product product={product} key={product.id} />;
+            })}
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default MensClothing;
